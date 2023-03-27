d277153d40888075004117c49f271c0b6417d29c
Ian <thecruik@gmail.com>
2023-03-26 21:13:40 -0700

    Format code (2)

--- ksa.vhd
+++ ksa.vhd
@@ -2,7 +2,7 @@ library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
--- Entity part of the description.  Describes inputs and outputs
+-- Entity part of the description. Describes inputs and outputs
 
 entity ksa is
     port(
@@ -18,9 +18,9 @@ end ksa;
 
 architecture rtl of ksa is
 
-   -- Declare the component for the ram.  This should match the entity description 
+    -- Declare the component for the ram. This should match the entity description 
 	-- in the entity created by the megawizard. If you followed the instructions in the 
-	-- handout exactly, it should match.  If not, look at s_memory.vhd and make the
+	-- handout exactly, it should match. If not, look at s_memory.vhd and make the
 	-- changes to the component below
 	
     COMPONENT s_memory IS
@@ -55,16 +55,16 @@ architecture rtl of ksa is
 	
 	component ksa_controller is
 		port(
-            clk_i 						: in  std_logic;
-			byte_array_256_done_i : in std_logic;	
-			byte_array_256_en_o   : out std_logic;
+            clk_i 				    : in  std_logic;
+			byte_array_256_done_i   : in  std_logic;	
+			byte_array_256_en_o     : out std_logic;
 			address_d_o   			: out std_logic_vector(7 downto 0)
         );
 	end component;
 
-	-- Enumerated type for the state variable.  You will likely be adding extra
+	-- Enumerated type for the state variable. You will likely be adding extra
 	-- state names here as you complete your design								
-   -- These are signals that are used to connect to the memory			
+    -- These are signals that are used to connect to the memory			
 
 	
     signal address : STD_LOGIC_VECTOR (7 DOWNTO 0);	 
@@ -73,22 +73,19 @@ architecture rtl of ksa is
     signal q : STD_LOGIC_VECTOR (7 DOWNTO 0);
 	 
 	 
-    -- data signals
-    signal secret_key : std_logic_vector(23 downto 0);
-    signal q_m : std_logic_vector(7 downto 0);
-    signal q_w	: std_logic;  
-    signal wren_w : std_logic;
-    signal wren_d	: std_logic;  
-    signal data_w	: std_logic_vector(7 downto 0);
-    signal data_d : std_logic_vector(7 downto 0);
-    signal address_w : std_logic_vector(7 downto 0);
-    signal address_m : std_logic_vector(7 downto 0);
+    -- Data signals
+    signal secret_key   : std_logic_vector(23 downto 0);
+    signal q_m          : std_logic_vector(7 downto 0);
+    signal q_w	        : std_logic;  
+    signal wren_w       : std_logic;
+    signal wren_d	    : std_logic;  
+    signal data_w	    : std_logic_vector(7 downto 0);
+    signal data_d       : std_logic_vector(7 downto 0);
+    signal address_w    : std_logic_vector(7 downto 0);
+    signal address_m    : std_logic_vector(7 downto 0);
 	 
 	 
-	 
-	 
-    -- controller signals
-    
+    -- Controller signals
     signal reset : std_logic;
     signal byte_array_256_done : std_logic;
     signal byte_array_256_en : std_logic;
@@ -97,17 +94,16 @@ architecture rtl of ksa is
 
 begin
     -- Include the S memory structurally
-
     u0 : s_memory
         port map(
-            address => address_w , 
+            address => address_w, 
             clock => CLOCK_50, 
             data => data_w, 
             wren => wren_w, 
             q => q
         );
         
-    -- write your code here.  As described in teh slide set, this 
+    -- write your code here. As described in the slide set, this 
     -- code will drive the address, data, and wren signals to
     -- fill the memory with the values 0...255
 
@@ -117,27 +113,27 @@ begin
 
     ksa_data0 : ksa_data
         port map(
-            clk_i => CLOCK_50,
-            secret_key_i => secret_key,
-            byte_array_256_en_i => byte_array_256_en,
-            q_m_i =>	q_m,
-            q_w_i =>	q_w,	  
-            wren_w_o	=>	wren_w,
-            wren_d_o => wren_d,	  
-            data_w_o =>	data_w,	
-            data_d_o	=>	data_d,
-            address_w_o => address_w,
-            address_d_o => address_d,
-            address_m_o => address_m,
-            byte_array_256_done_o => byte_array_256_done
+            clk_i                   => CLOCK_50,
+            secret_key_i            => secret_key,
+            byte_array_256_en_i     => byte_array_256_en,
+            q_m_i                   => q_m,
+            q_w_i                   => q_w,	  
+            wren_w_o	            => wren_w,
+            wren_d_o                => wren_d,	  
+            data_w_o                => data_w,	
+            data_d_o	            => data_d,
+            address_w_o             => address_w,
+            address_d_o             => address_d,
+            address_m_o             => address_m,
+            byte_array_256_done_o   => byte_array_256_done
         );	
 
     ksa_controller0 : ksa_controller 
         port map(
-            clk_i => CLOCK_50,
-            byte_array_256_done_i => byte_array_256_done,
-            byte_array_256_en_o  => byte_array_256_en,
-            address_d_o  => address_d   
+            clk_i                   => CLOCK_50,
+            byte_array_256_done_i   => byte_array_256_done,
+            byte_array_256_en_o     => byte_array_256_en,
+            address_d_o             => address_d   
         );
 
 end rtl;
--- ksa_controller.vhd
+++ ksa_controller.vhd
@@ -4,18 +4,23 @@ use ieee.numeric_std.all;
 
 
 entity ksa_controller is
-  port(clk_i 		: in  std_logic;  
-		 byte_array_256_done_i : in std_logic;	
-		 byte_array_256_en_o   : out std_logic;
-		 address_d_o   : out std_logic_vector(7 downto 0));
+    port(
+        clk_i 		            : in  std_logic;  
+        byte_array_256_done_i   : in std_logic;	
+        byte_array_256_en_o     : out std_logic;
+        address_d_o             : out std_logic_vector(7 downto 0)
+    );
 end ksa_controller;
-
 
 architecture behaviour of ksa_controller is
 
-type state_type is (INIT, DONE);
-signal current_state : state_type := INIT;
-signal next_state : state_type;
+    type state_type is (
+        INIT,
+        DONE
+    );
+
+    signal current_state : state_type := INIT;
+    signal next_state : state_type;
 
 begin
 
