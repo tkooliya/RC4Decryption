3278311ac982ed05570bff18bf9ace8dcdfd3440
Ian <thecruik@gmail.com>
2023-03-30 16:47:46 -0700

    Task 2b works with 1st message

--- ksa_controller.vhd
+++ ksa_controller.vhd
@@ -4,17 +4,26 @@ use ieee.numeric_std.all;
 
 entity ksa_controller is
     port(
-        clk_i       : in  std_logic;
-        rst_i       : in  std_logic;
-        fill_done_i : in  std_logic;
-        fill_o      : out std_logic;
+        clk_i               : in  std_logic;
+        rst_i               : in  std_logic;
+
+        fill_done_i         : in  std_logic;
+        fill_o              : out std_logic;
 		  
-		swap_done_i : in  std_logic; 
-		swap_read_i_o : out std_logic;
-		swap_compute_j_o  : out std_logic;
-        swap_read_j_o   : out std_logic;
-		swap_write_i_o  : out std_logic;
-		swap_write_j_o  : out std_logic
+		swap_done_i         : in  std_logic; 
+		swap_read_i_o       : out std_logic;
+		swap_compute_j_o    : out std_logic;
+        swap_read_j_o       : out std_logic;
+		swap_write_i_o      : out std_logic;
+		swap_write_j_o      : out std_logic;
+
+        decrypt_done_i      : in  std_logic;
+        decrypt_read_i_o    : out std_logic;
+        decrypt_read_j_o    : out std_logic;
+        decrypt_read_k_o    : out std_logic;
+        decrypt_write_k_o   : out std_logic;
+        decrypt_write_i_o   : out std_logic;
+        decrypt_write_j_o   : out std_logic
     );
 end entity;
 
@@ -30,7 +39,14 @@ architecture behaviour of ksa_controller is
         SWAP_READ_J,
 		SWAP_WRITE_I,
 		SWAP_WRITE_J,
-		  
+
+        DECRYPT_READ_I,
+        DECRYPT_READ_J,
+        DECRYPT_READ_K,
+        DECRYPT_WRITE_K,
+        DECRYPT_WRITE_I,
+        DECRYPT_WRITE_J,
+		
         DONE
     );
 	signal curr_state : state_t;
@@ -48,7 +64,12 @@ begin
     end process;
 
     -- next_state logic
-    process(curr_state, fill_done_i, swap_done_i) begin
+    process(
+        curr_state,
+        fill_done_i,
+        swap_done_i,
+        decrypt_done_i
+    ) begin
         next_state <= curr_state;
 
         case curr_state is
@@ -74,21 +95,50 @@ begin
 						
             when SWAP_WRITE_J =>
                 if(swap_done_i = '1') then
-                    next_state <= DONE;
+                    next_state <= DECRYPT_READ_I;
                 else
                     next_state <= SWAP_READ_I;
                 end if;
+
+            when DECRYPT_READ_I =>
+                next_state <= DECRYPT_READ_J;
+				
+            when DECRYPT_READ_J =>
+                next_state <= DECRYPT_READ_K;
+				
+            when DECRYPT_READ_K =>
+                next_state <= DECRYPT_WRITE_K;
+				
+            when DECRYPT_WRITE_K =>
+                next_state <= DECRYPT_WRITE_I;
+				
+            when DECRYPT_WRITE_I =>
+                next_state <= DECRYPT_WRITE_J;
+				
+            when DECRYPT_WRITE_J =>
+                if(decrypt_done_i = '1') then
+                    next_state <= DONE;
+                else
+                    next_state <= DECRYPT_READ_I;
+                end if;
 				
             when others =>
-				
         end case;
     end process;
 
     fill_o              <= '1' when (curr_state = FILL) else '0';
+
 	swap_read_i_o       <= '1' when (curr_state = SWAP_READ_I) else '0';
 	swap_compute_j_o    <= '1' when (curr_state = SWAP_COMPUTE_J) else '0';
     swap_read_j_o       <= '1' when (curr_state = SWAP_READ_J) else '0';
 	swap_write_i_o      <= '1' when (curr_state = SWAP_WRITE_I) else '0';
 	swap_write_j_o      <= '1' when (curr_state = SWAP_WRITE_J) else '0';
+
+    decrypt_read_i_o    <= '1' when (curr_state = DECRYPT_READ_I) else '0';
+    decrypt_read_j_o    <= '1' when (curr_state = DECRYPT_READ_J) else '0';
+    decrypt_read_k_o    <= '1' when (curr_state = DECRYPT_READ_K) else '0';
+    decrypt_write_k_o   <= '1' when (curr_state = DECRYPT_WRITE_K) else '0';
+    decrypt_write_i_o   <= '1' when (curr_state = DECRYPT_WRITE_I) else '0';
+    decrypt_write_j_o   <= '1' when (curr_state = DECRYPT_WRITE_J) else '0';
 
 end architecture;
--- ksa_datapath.vhd
+++ ksa_datapath.vhd
@@ -9,6 +9,8 @@ entity ksa_datapath is
 
         fill_i              : in std_logic;
         fill_done_o         : out std_logic;
+        
+        secret_key_i        : in std_logic_vector(23 downto 0);
 		  
 		swap_read_i_i       : in std_logic;
 		swap_compute_j_i    : in std_logic;
@@ -17,12 +19,26 @@ entity ksa_datapath is
 		swap_write_j_i      : in std_logic;
 		swap_done_o         : out std_logic;
 
-        secret_key_i        : in std_logic_vector(23 downto 0);
+        decrypt_read_i      : in std_logic;
+        decrypt_read_j      : in std_logic;
+        decrypt_read_k      : in std_logic;
+        decrypt_write_k     : in std_logic;
+        decrypt_write_i     : in std_logic;
+        decrypt_write_j     : in std_logic;
+        decrypt_done_o      : out std_logic;
 
-        wren_o              : out std_logic;
-        address_o           : out std_logic_vector(7 downto 0);
-        data_o              : out std_logic_vector(7 downto 0);
-		q_i	                : in std_logic_vector (7 downto 0)
+        wren_w_o            : out std_logic;
+        address_w_o         : out std_logic_vector(7 downto 0);
+        data_w_o            : out std_logic_vector(7 downto 0);
+		q_w_i	            : in std_logic_vector (7 downto 0);
+
+        address_rom_o       : out std_logic_vector(4 downto 0);
+        q_rom_i             : in std_logic_vector(7 downto 0);
+
+        wren_d_o            : out std_logic;
+        address_d_o         : out std_logic_vector(4 downto 0);
+        data_d_o            : out std_logic_vector(7 downto 0);
+		q_d_i	            : in std_logic_vector (7 downto 0)
     );
 end entity;
 
@@ -37,12 +53,22 @@ architecture behaviour of ksa_datapath is
     signal swap_temp_r      : unsigned(7 downto 0);
     signal swap_done        : std_logic;
     signal swap_next_j      : unsigned(7 downto 0);
-
     signal secret_key_byte_index   : integer;
 
+    -- Decrypt State signals
+    signal decrypt_i_r      : unsigned(7 downto 0);
+    signal decrypt_j_r      : unsigned(7 downto 0);
+    signal decrypt_k_r      : unsigned(4 downto 0);
+    signal decrypt_s_i_r    : unsigned(7 downto 0);
+    signal decrypt_s_j_r    : unsigned(7 downto 0);
+    signal decrypt_done     : std_logic;
+    signal decrypt_next_j   : unsigned(7 downto 0);
+
+    constant MESSAGE_LENGTH : integer := 32;
+    
 begin
 
-    -- Index filling register
+    -- Index filling logic
     process(clk_i, rst_i) begin
         if(rst_i = '1') then
             index_r <= to_unsigned(0, index_r'length);
@@ -54,9 +80,10 @@ begin
             end if;
         end if;
     end process;
+
+    fill_done_o <= '1' when (index_r = 255) else '0';
 	 
-	 
-	 -- Swap state registers
+	-- Swap state logic
     process(clk_i, rst_i) begin
         if(rst_i = '1') then
             swap_i_r    <= to_unsigned(0, swap_i_r'length);
@@ -66,7 +93,7 @@ begin
         elsif(rising_edge(clk_i)) then
             if(swap_compute_j_i = '1') then
                 swap_j_r <= swap_next_j;
-                swap_temp_r <= unsigned(q_i);
+                swap_temp_r <= unsigned(q_w_i);
 
             elsif(swap_write_j_i = '1') then
                 if(swap_done = '1') then
@@ -82,12 +109,39 @@ begin
     end process;
 
     secret_key_byte_index <= 2 - (to_integer(swap_i_r) mod 3);
-    swap_next_j <= (swap_j_r + unsigned(q_i) + unsigned(secret_key_i(8 * secret_key_byte_index + 7 downto 8 * secret_key_byte_index)));
+    swap_next_j <= (swap_j_r + unsigned(q_w_i) + unsigned(secret_key_i(8 * secret_key_byte_index + 7 downto 8 * secret_key_byte_index)));
 	 
-
-    fill_done_o <= '1' when (index_r = 255) else '0';
     swap_done   <= '1' when (swap_i_r = 255) else '0';
     swap_done_o <= swap_done;
+
+    -- Decrypt state logic
+    process(clk_i, rst_i) begin
+        if(rst_i = '1') then
+            decrypt_i_r     <= to_unsigned(0, 8);
+            decrypt_j_r     <= to_unsigned(0, 8);
+            decrypt_k_r     <= to_unsigned(0, decrypt_k_r'length);
+            decrypt_s_i_r   <= to_unsigned(0, 8);
+            decrypt_s_j_r   <= to_unsigned(0, 8);
+        elsif(rising_edge(clk_i)) then
+            if(decrypt_read_i = '1') then
+                decrypt_i_r <= decrypt_i_r + to_unsigned(1, 8);
+
+            elsif(decrypt_read_j = '1') then
+                decrypt_s_i_r <= unsigned(q_w_i);
+                decrypt_j_r <= decrypt_next_j;
+
+            elsif(decrypt_read_k = '1') then
+                decrypt_s_j_r <= unsigned(q_w_i);
+
+            elsif(decrypt_write_j = '1') then
+                decrypt_k_r <= decrypt_k_r + to_unsigned(1, decrypt_k_r'length);
+            end if;
+        end if;
+    end process;
+
+    decrypt_done <= '1' when (decrypt_k_r = 31) else '0';
+    decrypt_done_o <= decrypt_done;
+    decrypt_next_j <= decrypt_j_r + unsigned(q_w_i);
 
     -- Memory signal control
     process(
@@ -99,35 +153,79 @@ begin
         swap_write_j_i,
         index_r,
         swap_i_r,
-        q_i,
         swap_j_r,
-        swap_temp_r
+        swap_temp_r,
+        q_w_i,
+        decrypt_read_i,
+        decrypt_read_j,
+        decrypt_read_k,
+        decrypt_write_k,
+        decrypt_write_i,
+        decrypt_write_j,
+        decrypt_i_r,
+        decrypt_j_r,
+        decrypt_next_j,
+        decrypt_k_r,
+        decrypt_s_i_r,
+        decrypt_s_j_r,
+        q_rom_i
     ) begin
-        wren_o      <= '0';
-        address_o   <= "00000000";
-        data_o      <= "00000000";
+        wren_w_o        <= '0';
+        address_w_o     <= "00000000";
+        data_w_o        <= "00000000";
+        
+        address_rom_o   <= "00000";
+        
+        wren_d_o        <= '0';
+        address_d_o     <= "00000";
+        data_d_o        <= "00000000";
 
         if(fill_i = '1') then
-            wren_o      <= '1';
-            address_o   <= std_logic_vector(index_r);
-            data_o      <= std_logic_vector(index_r);
+            wren_w_o        <= '1';
+            address_w_o     <= std_logic_vector(index_r);
+            data_w_o        <= std_logic_vector(index_r);
 
         elsif(swap_read_i_i = '1') then
-            address_o   <= std_logic_vector(swap_i_r);
+            address_w_o     <= std_logic_vector(swap_i_r);
 
         elsif(swap_read_j_i = '1') then
-            address_o   <= std_logic_vector(swap_j_r);
+            address_w_o     <= std_logic_vector(swap_j_r);
 
         elsif(swap_write_i_i = '1') then
-            wren_o      <= '1';
-            address_o   <= std_logic_vector(swap_i_r);
-            data_o      <= std_logic_vector(q_i);
+            wren_w_o        <= '1';
+            address_w_o     <= std_logic_vector(swap_i_r);
+            data_w_o        <= std_logic_vector(q_w_i);
 
         elsif(swap_write_j_i = '1') then
-            wren_o      <= '1';
-            address_o   <= std_logic_vector(swap_j_r);
-            data_o      <= std_logic_vector(swap_temp_r);
+            wren_w_o        <= '1';
+            address_w_o     <= std_logic_vector(swap_j_r);
+            data_w_o        <= std_logic_vector(swap_temp_r);
+
+        elsif(decrypt_read_i = '1') then
+            address_w_o     <= std_logic_vector(decrypt_i_r + to_unsigned(1, 8));
+
+        elsif(decrypt_read_j = '1') then
+            address_w_o     <= std_logic_vector(decrypt_next_j);
+
+        elsif(decrypt_read_k = '1') then
+            address_w_o     <= std_logic_vector(decrypt_s_i_r + unsigned(q_w_i));
+            address_rom_o   <= std_logic_vector(decrypt_k_r);
+
+        elsif(decrypt_write_k = '1') then
+            wren_d_o        <= '1';
+            address_d_o     <= std_logic_vector(decrypt_k_r);
+            data_d_o        <= std_logic_vector(q_w_i) xor std_logic_vector(q_rom_i);
+
+        elsif(decrypt_write_i = '1') then
+            wren_w_o        <= '1';
+            address_w_o     <= std_logic_vector(decrypt_i_r);
+            data_w_o        <= std_logic_vector(decrypt_s_j_r);
+
+        elsif(decrypt_write_j = '1') then
+            wren_w_o        <= '1';
+            address_w_o     <= std_logic_vector(decrypt_j_r);
+            data_w_o        <= std_logic_vector(decrypt_s_i_r);
+
         end if;
     end process;
-
 end architecture;
